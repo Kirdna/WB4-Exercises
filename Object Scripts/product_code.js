52fb329051170d39bc499275fb0a535833786166
Nerio Guio <andrik1379@gmail.com>
2023-05-08 14:10:47 -0400

    first part of the workbook

---
+++ Object Scripts/product_code.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function parsePartCode(code){
+    
+
+
+
+
+    let result = {
+        supplierCode: getSupplierCode(code),
+        productNumber: getProductNumber(code),
+        size: getSize(code)
+    }
+
+    return result;
+}
+
+function getSupplierCode(code){
+    let addressOfColon = code.indexOf(":");
+    let result = code.substring(0, addressOfColon);
+    return result;
+
+}
+
+function getProductNumber(code){
+    let addressOfColon = code.indexOf(":");
+    let addressOfDash = code.indexOf("-");
+    let result = code.substring(addressOfColon, addressOfDash);
+    return result;
+
+}
+
+function getSize(code){
+    let addressOfDash = code.indexOf("-");
+    let result = code.substring(addressOfDash + 1);
+    return result;
+
+}
+
+function displayPart(part){
+    console.log(`SupplierCode: ${part.supplierCode}`);
+    console.log(`ProductNumber: ${part.productNumber}`);
+    console.log(`Size: ${part.size}`);
+}
+
+let code1 = "ACME:123-L";
+let code2 = "DI:12345-M";
+let code3 = "ACE:1=12";
+
+let part1 = parsePartCode(code1);
+displayPart(part1);
+
+let part2 = parsePartCode(code2);
+displayPart(part2);
---
+++ examples/1-7.js
@@ -0,0 +1,15 @@
+let employeeName = "Ezra";
+let employeeJobTitle = "Teacher";
+let employeePayRate = 19.99;
+
+function DisplayEmployeeInfo(name, jobTitle, payRate){
+    console.log(`The employee named ${name} works as a ${jobTitle} and makes ${payRate}.`);
+}
+
+DisplayEmployeeInfo(employeeName, employeeJobTitle, employeePayRate);
+
+let emp3 = {
+    name: "Matt",
+    jobTitle: "Instructor",
+    payRate: 19.99
+};
